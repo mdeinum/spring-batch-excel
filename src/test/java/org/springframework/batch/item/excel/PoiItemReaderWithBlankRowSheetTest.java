a960458b34d6ad41b25b4c7e48c409e50a3f4ea0
Marten Deinum <mdeinum@gmail.com>
2019-01-07 14:15:23 +0100

    polish

--- src/test/java/org/springframework/batch/item/excel/PoiItemReaderWithBlankRowSheetTest.java
+++ src/test/java/org/springframework/batch/item/excel/PoiItemReaderWithBlankRowSheetTest.java
@@ -6,6 +6,8 @@ package org.springframework.batch.item.excel;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Before;
@@ -13,36 +15,28 @@ import org.junit.Test;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.excel.mapping.PassThroughRowMapper;
 import org.springframework.batch.item.excel.poi.PoiItemReader;
-import org.springframework.batch.item.excel.support.rowset.RowSet;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.util.StringUtils;
 
 /**
  * Created by mishrk3 on 3/17/2016.
  */
-public class PoiItemReaderWithBlankRowSheet {
+public class PoiItemReaderWithBlankRowSheetTest {
 
-	protected final Log logger = LogFactory.getLog(this.getClass());
+	private final Log logger = LogFactory.getLog(this.getClass());
 
-	protected AbstractExcelItemReader itemReader;
-
-	private ExecutionContext executionContext;
+	private AbstractExcelItemReader<String[]> itemReader;
 
 	@Before
 	public void setup() throws Exception {
-		this.itemReader = createExcelItemReader();
+		this.itemReader = new PoiItemReader<>();
+		this.itemReader.setResource(new ClassPathResource("blankRow.xlsx"));
 		this.itemReader.setLinesToSkip(1); // First line is column names
 		this.itemReader.setRowMapper(new PassThroughRowMapper());
-		this.itemReader.setSkippedRowsCallback(new RowCallbackHandler() {
-
-			public void handleRow(RowSet rs) {
-				logger.info("Skipping: " + StringUtils.arrayToCommaDelimitedString(rs.getCurrentRow()));
-			}
-		});
-		configureItemReaderWithBlankRowSheet(this.itemReader);
+		this.itemReader.setSkippedRowsCallback(rs -> logger.info("Skipping: " + Arrays.toString(rs.getCurrentRow())));
 		this.itemReader.afterPropertiesSet();
-		executionContext = new ExecutionContext();
+
+		ExecutionContext executionContext = new ExecutionContext();
 		this.itemReader.open(executionContext);
 	}
 
@@ -51,21 +45,13 @@ public class PoiItemReaderWithBlankRowSheet {
 		assertEquals(1, this.itemReader.getNumberOfSheets());
 		String[] row;
 		do {
-			row = (String[]) this.itemReader.read();
-			this.logger.debug("Read: " + StringUtils.arrayToCommaDelimitedString(row));
+			row = this.itemReader.read();
+			this.logger.debug("Read: " + Arrays.toString(row));
 			if (row != null) {
 				assertEquals(4, row.length);
 			}
 		} while (row != null);
 		int readCount = (Integer) ReflectionTestUtils.getField(this.itemReader, "currentItemCount");
 		assertEquals(7, readCount);
-	}
-
-	protected AbstractExcelItemReader createExcelItemReader() {
-		return new PoiItemReader();
-	}
-
-	protected void configureItemReaderWithBlankRowSheet(AbstractExcelItemReader itemReader) {
-		itemReader.setResource(new ClassPathResource("org/springframework/batch/item/excel/blankRow.xlsx"));
 	}
 }
