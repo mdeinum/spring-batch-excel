342a548b41c6c94721ccd1e63203cfdbf2c22e0e
Marten Deinum <mdeinum@gmail.com>
2020-11-11 13:09:49 +0100

    Polish Javadoc

--- src/main/java/biz/deinum/batch/item/excel/RowCallbackHandler.java
+++ src/main/java/biz/deinum/batch/item/excel/RowCallbackHandler.java
@@ -25,6 +25,17 @@ import biz.deinum.batch.item.excel.support.rowset.RowSet;
  */
 public interface RowCallbackHandler {
 
+	/**
+	 * Implementations must implement this method to process each row of data in the {@code RowSet}.
+	 *
+	 * This method should not call {@code next()} on the {@code RowSetSet}; it is only supposed to extract values of the
+	 * current row.
+	 *
+	 * Exactly what the implementation chooses to do is up to it: A trivial implementation might simply count rows,
+	 * while another implementation might build a special header row.
+	 *
+	 * @param rs the {@code RowSet} to process (preset at the current row)
+	 */
     void handleRow(RowSet rs);
 
 }
